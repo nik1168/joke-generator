61b816ed7cd15ff9fcd4a71a55ad471d0e5dc6c7
nik1168 <ngeisser32@gmail.com>
2022-11-28 21:45:28 -0400

    feat: add jokes generator until auth

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,45 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction } from "@remix-run/node";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    Links,
+    LiveReload,
+    Outlet,
 } from "@remix-run/react";
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+import globalStylesUrl from "./styles/global.css";
+import globalMediumStylesUrl from "./styles/global-medium.css";
+import globalLargeStylesUrl from "./styles/global-large.css";
+
+export const links: LinksFunction = () => {
+    return [
+        {
+            rel: "stylesheet",
+            href: globalStylesUrl,
+        },
+        {
+            rel: "stylesheet",
+            href: globalMediumStylesUrl,
+            media: "print, (min-width: 640px)",
+        },
+        {
+            rel: "stylesheet",
+            href: globalLargeStylesUrl,
+            media: "screen and (min-width: 1024px)",
+        },
+    ];
+};
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8" />
+            <title>Remix: So great, it's funny!</title>
+            <Links />
+        </head>
+        <body>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
         <LiveReload />
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
